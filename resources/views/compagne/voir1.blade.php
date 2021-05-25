1957c8c33f1df514141fc0f09cc7e5a303c4e04e
fatima-wedigitalpro <79646464+fatima-wedigitalpro@users.noreply.github.com>
2021-05-25 18:51:32 +0100

    commit

--- resources/views/compagne/voir1.blade.php
+++ resources/views/compagne/voir1.blade.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 <center>
 <br><br><br>
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center"> 
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Organizations') }}</div>
