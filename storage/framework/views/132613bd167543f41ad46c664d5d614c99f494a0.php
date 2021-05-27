ce2d9704933f050bb621df4ee6310578f9b0b786
fatima-wedigitalpro <79646464+fatima-wedigitalpro@users.noreply.github.com>
2021-05-27 17:47:26 +0100

    new commmit

---
+++ storage/framework/views/132613bd167543f41ad46c664d5d614c99f494a0.php
@@ -0,0 +1,33 @@
+
+
+<?php $__env->startSection('content'); ?>
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!--Bootstrap-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+        <!-- Styles -->
+       
+<style>
+body  {
+    background-image: url("https://cdn.hipwallpaper.com/i/52/70/78IzCX.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
+
+    </head>
+    <body>
+    </body>
+</html>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\dell\Documents\GitHub\Appli\resources\views/welcome.blade.php ENDPATH**/ ?>
